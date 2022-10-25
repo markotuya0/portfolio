ec7fa1a224a4aa4b84a6ae58c80212d49ebfde9c
Mark Otuya <markotuya0@gmail.com>
2022-10-25 02:10:18 +0100

    finished

---
+++ src/data.js
@@ -0,0 +1,68 @@
+export const projects = [
+  {
+    title: "Bookstore App",
+    subtitle: "React-Redux & Javascript",
+    description:
+      "In this project, we used React and Redux to build a simple application. The project was a real-world project for a fictional company that required our help. The project got split into multiple milestones, and each had functional and technical requirements that all got implemented.",
+    image: "./book.gif",
+    link: "https://mark-react-bookstore.netlify.app/",
+  },
+  {
+    title: "Space Traveler's Hub",
+    subtitle: "React-Redux & Javascript",
+    description:
+      "This is React-Redux space-travelrs  project, and this project is about building a simple yet complicated web application based on an external space x API, each had functional and technical requirements that all got implemented.built with React- Redux and javascript.",
+    image: "./space.gif",
+    link: "https://delightful-starship-b08d3e.netlify.app/",
+  },
+  {
+    title: "TO-DO List",
+    subtitle: "React-redux & javascript",
+    description:
+      "This is the  Todo list project using html + css + modular javascript, ES6 syntax, and Webpack.I used hooks to handle state. Invalid operation errors are successfully handled through useEffect.      ",
+    image: "./todo.gif",
+    link: "https://mark-todolist.netlify.app/",
+  },
+  {
+    title: "Maths Magician",
+    subtitle: "React-Redux & javascript",
+    description:
+      "This is a Calculator I built with React employing hooks and best practices, such as lift state, destructuring, and passing props to child components.",
+    image: "./maths.gif",
+    link: "https://marks-math-magician.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "HTML5",
+  "CSS3",
+  "JAVASCRIPT",
+  "REACT- REDUX",
+  "NEXT.JS",
+  "NODE",
+  "RUBY-RAILS",
+  "SQL-POSTGRESSQL",
+  "DATABASE MANAGEMENT",
+  "PROBLEM SOLVING",
+  "LOGICAL REASONING & OBJECTIVITY",
+  "TEAM WORK & COLLABORATION",
+  "SELF DRIVEN",
+  "TIME MANAGEMENT",
+];
